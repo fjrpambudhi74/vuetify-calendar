afb08f6a8f3033da5f5b5dacc37e205d4fce0f0e
fajarpambudhi <fjrpambudhi@hotmail.com>
2020-10-15 20:12:30 +0700

    Initial Commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,313 @@
+<template>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-toolbar flat>
+          <v-btn class="mr-4" color="primary" @click="dialog = true" dark>
+            New Event
+          </v-btn>
+          <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+            Today
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="prev">
+            <v-icon small> mdi-chevron-left </v-icon>
+          </v-btn>
+          <v-btn fab text small color="grey darken-2" @click="next">
+            <v-icon small> mdi-chevron-right </v-icon>
+          </v-btn>
+          <v-toolbar-title>
+            {{ title }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu bottom right>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right> mdi-menu-down </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Day</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Week</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Month</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = '4day'">
+                <v-list-item-title>4 days</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </v-sheet>
+
+      <!-- Add Event Dialog  -->
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field
+                v-model="name"
+                type="text"
+                label="Event name (required)"
+              ></v-text-field>
+              <v-text-field
+                v-model="details"
+                type="text"
+                label="Detail"
+              ></v-text-field>
+              <v-text-field
+                v-model="start"
+                type="date"
+                label="Start (required)"
+              ></v-text-field>
+              <v-text-field
+                v-model="end"
+                type="date"
+                label="End (required)"
+              ></v-text-field>
+              <v-text-field
+                v-model="color"
+                type="color"
+                label="Color (click to open color menu)"
+              ></v-text-field>
+              <v-btn
+                type="submit"
+                color="primary"
+                class="mr-4"
+                @click.stop="dialog = false"
+                >Create Event</v-btn
+              >
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
+      <!-- End Event Dialog -->
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          v-model="focus"
+          color="primary"
+          :events="events"
+          :event-color="getEventColor"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          @change="updateRange"
+        ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card color="grey lighten-4" min-width="350px" flat>
+            <v-toolbar :color="selectedEvent.color" dark>
+              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <form v-if="currentlyEditing !== selectedEvent.id">
+                {{ selectedEvent.details }}
+              </form>
+              <form v-else>
+                <textarea-autosize
+                  v-model="selectedEvent.details"
+                  type="text"
+                  style="width: 100%"
+                  :min-height="100"
+                  placeholder="Add Note"
+                >
+                </textarea-autosize>
+              </form>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="secondary" @click="selectedOpen = false">
+                Close
+              </v-btn>
+              <v-btn
+                text
+                v-if="currentlyEditing !== selectedEvent.id"
+                @click.prevent="editEvent(selectedEvent)"
+              >
+                Edit
+              </v-btn>
+              <v-btn text v-else @click.prevent="updateEvent(selectedEvent)">
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { db } from "@/main";
+
+export default {
+  /* eslint-disable */
+  data() {
+    return {
+      today: new Date().toISOString().substr(0, 10),
+      focus: new Date().toISOString().substr(0, 10),
+      type: "month",
+      typeToLabel: {
+        month: "Month",
+        week: "Week",
+        day: "Day",
+        "4day": "4 Days",
+      },
+      name: null,
+      details: null,
+      start: null,
+      end: null,
+      color: "#F48FB1" /* Default event color */,
+      currentlyEditing: null,
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      events: [],
+      dialog: false,
+    };
+  },
+  mounted() {
+    this.getEvents();
+  },
+  computed: {
+    //   Range title for sorting list date
+    title() {
+      const { start, end } = this;
+      if (!start || !end) {
+        return "";
+      }
+      const startMonth = this.monthFormatter(start);
+      const endMonth = this.monthFormatter(end);
+      const suffixMonth = startMonth === endMonth ? "" : endMonth;
+      const startYear = start.year;
+      const endYear = end.year;
+      const suffixYear = startYear === endYear ? "" : endYear;
+      const startDay = start.day + this.nth(start.day);
+      const endDay = end.day + this.nth(end.day);
+      switch (this.type) {
+        case "month":
+          return `${startMonth} ${startYear}`;
+        case "week":
+        case "4day":
+          return `${startMonth} ${startDay} ${startYear} - ${suffixMonth} ${endDay} ${suffixYear}`;
+        case "day":
+          return `${startMonth} ${startDay} ${startYear}`;
+      }
+      return "";
+    },
+    monthFormatter() {
+      return this.$refs.calendar.getFormatter({
+        timeZone: "UTC",
+        month: "long",
+      });
+    },
+  },
+  methods: {
+    async getEvents() {
+      let snapshot = await db.collection("calEvent").get();
+      let events = [];
+
+      snapshot.forEach((doc) => {
+        let appData = doc.data();
+        appData.id = doc.id;
+        events.push(appData);
+      });
+      this.events = events;
+    },
+    async addEvent() {
+      if (this.name && this.start && this.end) {
+        await db.collection("calEvent").add({
+          name: this.name,
+          details: this.details,
+          start: this.start,
+          end: this.end,
+          color: this.color,
+        });
+        this.getEvents();
+        this.name = "";
+        this.details = "";
+        this.start = "";
+        this.end = "";
+        this.color = "#F48FB1";
+      } else {
+        alert("Please Fill In The Blanks");
+      }
+    },
+    async updateEvent(ev) {
+      await db.collection("calEvent").doc(this.currentlyEditing).update({
+        details: ev.details,
+      });
+      this.selectedOpen = false;
+      this.currentlyEditing = null;
+    },
+    async deleteEvent(ev) {
+      await db.collection("calEvent").doc(ev).delete();
+      this.selectedOpen = false;
+      this.getEvents();
+    },
+    viewDay({ date }) {
+      this.focus = date;
+      this.type = "day";
+    },
+    getEventColor(event) {
+      return event.color;
+    },
+    setToday() {
+      this.focus = "";
+    },
+    prev() {
+      this.$refs.calendar.prev();
+    },
+    next() {
+      this.$refs.calendar.next();
+    },
+    editEvent(ev) {
+      this.currentlyEditing = ev.id;
+    },
+    showEvent({ nativeEvent, event }) {
+      const open = () => {
+        this.selectedEvent = event;
+        this.selectedElement = nativeEvent.target;
+        setTimeout(() => {
+          this.selectedOpen = true;
+        }, 10);
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    },
+    updateRange({ start, end }) {
+      this.start = start;
+      this.end = end;
+    },
+    nth(d) {
+      return d > 3 && d < 21
+        ? "th"
+        : ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th"][d % 10];
+    },
+  },
+  /* eslint-enable */
+};
+</script>
+
+<style >
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDUvoa9zXnhyxkVN67q3laILa_xM2BbCEw",
+  authDomain: "vuetify-calendar-909d9.firebaseapp.com",
+  databaseURL: "https://vuetify-calendar-909d9.firebaseio.com",
+  projectId: "vuetify-calendar-909d9",
+  storageBucket: "vuetify-calendar-909d9.appspot.com",
+  messagingSenderId: "1062159275123",
+  appId: "1:1062159275123:web:9332addbd5007529774606"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
